5aa52ce6b7891922cb83b531b2eb43ec2ab3011f
Sultanxda <sultanxda@gmail.com>
2017-04-25 14:41:09 -0700

    scsi: sd: Configure the I/O sched switcher for the sde block device
    This enables the I/O scheduler switcher driver to manipulate the current
    I/O scheduler used for the sde block device.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: khusika <khusikadhamar@gmail.com>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -51,6 +51,7 @@
 #include <linux/async.h>
 #include <linux/slab.h>
 #include <linux/pm_runtime.h>
+#include <linux/iosched_switcher.h>
 #include <linux/pr.h>
 #include <linux/t10-pi.h>
 #include <asm/uaccess.h>
@@ -3048,6 +3049,9 @@ static int sd_probe(struct device *dev)
 
 	get_device(&sdkp->dev);	/* prevent release before async_schedule */
 	async_schedule_domain(sd_probe_async, sdkp, &scsi_sd_probe_domain);
+
+	if (!strcmp(sdkp->disk->disk_name, "sde"))
+		init_iosched_switcher(sdp->request_queue);
 
 	return 0;
 
